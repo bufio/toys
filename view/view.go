c2f314cadb91b0a1b8ddee9db8b938f79fa09e64
nguyen <nguyen@open-vn.org>
2012-12-16 11:42:03 +0700

    view: add shared template function feature

--- view/view.go
+++ view/view.go
@@ -3,8 +3,10 @@ package view
 import (
 	"fmt"
 	"html/template"
+	"io/ioutil"
 	"os"
 	"path/filepath"
+	"reflect"
 )
 
 type Writer interface {
@@ -23,9 +25,28 @@ func NewViewSet() *ViewSet {
 }
 
 type View struct {
-	Root    string
-	Set     map[string]*ViewSet
-	current string
+	Root     string
+	Set      map[string]*ViewSet
+	current  string
+	funcsMap template.FuncMap
+}
+
+func (v *View) AddFunc(name string, f interface{}) error {
+	if r := reflect.TypeOf(f); r.Kind() == reflect.Func {
+		if r.NumOut() > 2 {
+			return fmt.Errorf("view: %s", "function must have no more than 2 output parameter")
+		}
+		if r.NumOut() == 2 {
+			o := r.Out(1)
+			_, ok := o.MethodByName("Error")
+			if !ok {
+				return fmt.Errorf("view: %s", "function must have the last output parameter implements error")
+			}
+		}
+		v.funcsMap[name] = f
+		return nil
+	}
+	return fmt.Errorf("view: %s", "AddFunc require a valid function")
 }
 
 func (v *View) Parse(set string) error {
@@ -36,10 +57,10 @@ func (v *View) Parse(set string) error {
 	}
 
 	setFolder := filepath.Join(v.Root, set)
-	tmpl := template.Must(template.ParseGlob(filepath.Join(setFolder, "shared", "*.tmpl")))
+
+	tmpl := template.Must(template.New("layout.tmpl").Funcs(v.funcsMap).ParseGlob(filepath.Join(setFolder, "shared", "*.tmpl")))
 	vs := NewViewSet()
 	vs.diver = tmpl
-
 	//parse page
 	root, err := os.Open(setFolder)
 	if err != nil {
@@ -56,8 +77,12 @@ func (v *View) Parse(set string) error {
 			if err != nil {
 				continue
 			}
-
-			_, err = p.ParseFiles(filepath.Join(setFolder, file.Name()))
+			//read file
+			b, err := ioutil.ReadFile(filepath.Join(setFolder, file.Name()))
+			if err != nil {
+				continue
+			}
+			_, err = p.Parse(string(b))
 			if err == nil {
 				vs.page[file.Name()] = p
 			}
@@ -72,15 +97,17 @@ func (v *View) Parse(set string) error {
 func (v *View) Load(w Writer, pageName string, data interface{}) {
 	p, ok := v.Set[v.current].page[pageName]
 	if ok {
-		p.Execute(w, data)
+		p.ExecuteTemplate(w, "layout.tmpl", data)
 		return
 	}
-	fmt.Fprintf(w, "data: %#v", data)
+	fmt.Fprintf(w, "%#v", data)
 }
 
 func NewView(root string) *View {
 	v := &View{}
 	v.Root = root
 	v.Set = make(map[string]*ViewSet)
+	v.funcsMap = template.FuncMap{}
+
 	return v
 }
