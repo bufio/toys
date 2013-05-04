9a007a6cd13e8afd35cd373fbfa9573dd19ec37d
nvcnvn <nguyen@open-vn.org>
2013-05-04 23:12:55 +0700

    Documenting code for main pkg and view pkg Add christmas view-set to example/toysign.

--- toys.go
+++ toys.go
@@ -1,6 +1,7 @@
 // Copyright 2012 The Toys Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
+
 package toys
 
 import (
@@ -11,50 +12,70 @@ import (
 	"path"
 )
 
+type InfoKey uint8
+
+const (
+	RequestMethod InfoKey = iota
+	RequestHost
+	RequestPath
+	RequestQuery
+	RemoteAddress
+)
+
+// Controller is the main struct of toys framework. Use it to handle the request.
 type Controller struct {
 	req   *http.Request
 	respw http.ResponseWriter
-	inf   map[string]string
+	inf   map[InfoKey]string
 	path  string
 }
 
+// Init initial the Controller given a http.ResponseWriter and *http.Request. You must call Init
+// right after create new Controller for each request.
 func (c *Controller) Init(w http.ResponseWriter, r *http.Request) {
 	c.req = r
 	c.respw = w
-	c.inf = make(map[string]string)
-	c.inf["req_method"] = r.Method
-	c.inf["req_host"] = r.URL.Host
-	c.inf["req_path"] = r.URL.Path
-	c.inf["req_query"] = r.URL.RawQuery
-	c.inf["remote_addr"] = r.RemoteAddr
+	c.inf = make(map[InfoKey]string)
+	c.inf[RequestMethod] = r.Method
+	c.inf[RequestHost] = r.URL.Host
+	c.inf[RequestPath] = r.URL.Path
+	c.inf[RequestQuery] = r.URL.RawQuery
+	c.inf[RemoteAddress] = r.RemoteAddr
 }
 
+// Write writes the slice of bytes b to the web browser.
 func (c *Controller) Write(b []byte) (int, error) {
 	return c.respw.Write(b)
 }
 
+// Request returns the origin *http.Request
 func (c *Controller) Request() *http.Request {
 	return c.req
 }
 
+// Redirect send the redirect header with the url destination and the status code.
 func (c *Controller) Redirect(url string, code int) {
 	http.Redirect(c.respw, c.req, url, code)
 }
 
-func (c *Controller) Info(key string) string {
+func (c *Controller) Info(key InfoKey) string {
 	return c.inf[key]
 }
 
+// POST returns the string value for the named component of the POST or GET query. It call
+// template.HTMLEscapeString for the output if filter is true.
 func (c *Controller) Post(name string, filter bool) string {
-	if c.req.Method == "POST" {
+	if c.req.Method == "POST" || c.req.Method == "PUT" {
 		if filter {
-			return template.HTMLEscapeString(c.req.FormValue(name))
+			return template.HTMLEscapeString(c.req.PostFormValue(name))
 		}
-		return c.req.FormValue(name)
+		return c.req.PostFormValue(name)
 	}
 	return ""
 }
 
+// Get returns the first value for the named component of the GET or HEAD query. It call
+// template.HTMLEscapeString for the output if filter is true.
 func (c *Controller) Get(name string, filter bool) string {
 	if c.req.Method == "GET" || c.req.Method == "HEAD" {
 		if filter {
@@ -65,6 +86,8 @@ func (c *Controller) Get(name string, filter bool) string {
 	return ""
 }
 
+// Cookie return the cookis value with given name. It call template.HTMLEscapeString for the output
+// if filter is true.
 func (c *Controller) Cookie(name string, filter bool) string {
 	cookie, err := c.req.Cookie(name)
 	if err != nil {
@@ -76,18 +99,26 @@ func (c *Controller) Cookie(name string, filter bool) string {
 	return ""
 }
 
+// Print formats using the default formats for its operands and writes to web browser. It returns
+// the number of bytes written and any write error encountered.
 func (c *Controller) Print(a ...interface{}) {
 	fmt.Fprint(c.respw, a...)
 }
 
+// Printf formats according to a format specifier and writes to web browser. It returns the number
+// of bytes written and any write error encountered.
 func (c *Controller) Printf(format string, a ...interface{}) {
 	fmt.Fprintf(c.respw, format, a...)
 }
 
+// SetPath sets path of the application. For example, if you want you application handle the address
+// example.com/toysapp/ then the input for SetPath should be "/toysapp/".
 func (c *Controller) SetPath(p string) {
 	c.path = p
 }
 
+// BasePath return the relative url base on the path seted with SetPath. BasePath will return the
+// origin value if p is an absolute url.
 func (c *Controller) BasePath(p string) string {
 	path_url, err := url.Parse(p)
 	if err != nil && path_url.IsAbs() {
--- view/view.go
+++ view/view.go
@@ -1,6 +1,41 @@
 // Copyright 2012 The Toys Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
+
+/*
+Package view implements a basic template system on top of html/template package.
+The package require a special structed folder like this example:
+
+	path/to/template/
+	├── christmas
+	│   ├── page1.tmpl
+	│   ├── page2.tmpl
+	│   └── shared
+	│       ├── menu.tmpl
+	│       └── layout.tmpl
+	└── default
+	    ├── page1.tmpl
+	    ├── page2.tmpl
+	    └── shared
+	        ├── menu.tmpl
+	        └── layout.tmpl
+
+Assuming your template folder located at path/to/template. There is some rules with this folder:
+
+	In the template folder should contain some sub-folder called view-set.
+	Each view-set shloud contain some xyz.tmpl (where xyz is the file name) and a folder name "shared".
+	In the "shared" folder must contain a file "layout.tmpl" (all the template file must end with .tmpl).
+
+There is some rules for "xyz.tmpl" files:
+
+	All the contain of the file must in the {{define "page"}} ... {{end}} block.
+	You can call the shared content (insert the content of the tmpl file in shared folder) by {{template "menu.tmpl"}} etc.
+
+The "layout.tmpl" in shared folder is the main layout. The content of "xyz.tmpl" files should be
+embedded in this file. You must put {{template "page" .}} some where in this file.
+
+For more detail, see the tutorial.
+*/
 package view
 
 import (
@@ -17,17 +52,20 @@ import (
 	"reflect"
 )
 
+// ViewSet represents the view-set sub folder.
 type ViewSet struct {
 	diver *template.Template
 	page  map[string]*template.Template
 }
 
+// NewViewSet return a new ViewSet
 func NewViewSet() *ViewSet {
 	v := &ViewSet{}
 	v.page = make(map[string]*template.Template)
 	return v
 }
 
+// View manages the whole template system.
 type View struct {
 	root     string
 	set      map[string]*ViewSet
@@ -36,6 +74,7 @@ type View struct {
 	resource string
 }
 
+// NewView returns a new View with the given location of the template folder.
 func NewView(root string) *View {
 	v := &View{}
 	v.root = root
@@ -60,12 +99,22 @@ func NewView(root string) *View {
 	return v
 }
 
+/*
+HandleResource make a handler that serves HTTP for static file that use for template system.
+For example if you want handle the static files at example.com/statics/ you should call:
+	*View.HandleResource("/statics/", "path/to/statics/folder/")
+And then in the .tmpl file you can call {{resource "css/index.css"}}, it will returns
+"/statics/css/index.css"
+*/
 func (v *View) HandleResource(prefix, folder string) {
 	v.resource = prefix
 	http.Handle(prefix, http.StripPrefix(prefix,
 		http.FileServer(http.Dir(folder))))
 }
 
+// AddFunc add the function to the template system. You call call the function you added in the .tmpl
+// files by {{function-name}}. An error return if you add an invalid function.
+// Note: this function must be call before Parse.
 func (v *View) AddFunc(name string, f interface{}) error {
 	if r := reflect.TypeOf(f); r.Kind() == reflect.Func {
 		if r.NumOut() > 2 {
@@ -84,6 +133,7 @@ func (v *View) AddFunc(name string, f interface{}) error {
 	return fmt.Errorf("view: %s", "AddFunc require a valid function")
 }
 
+// SetDefault change the set to default. It call Parse if need.
 func (v *View) SetDefault(set string) error {
 	_, ok := v.set[set]
 	if !ok {
@@ -96,6 +146,8 @@ func (v *View) SetDefault(set string) error {
 	return nil
 }
 
+// Parse parses the view-set you want to use. You may call Parse for all view-set you have and then
+// switching beetwen them by call SetDefault.
 func (v *View) Parse(set string) error {
 	setFolder := filepath.Join(v.root, set)
 
@@ -140,6 +192,7 @@ func (v *View) Parse(set string) error {
 	return nil
 }
 
+// Load render the template you specific with name and write it to the Writer.W
 func (v *View) Load(w io.Writer, pageName string, data interface{}) error {
 	p, ok := v.set[v.current].page[pageName]
 	if ok {
@@ -149,6 +202,10 @@ func (v *View) Load(w io.Writer, pageName string, data interface{}) error {
 	return errors.New("view: cannot load template")
 }
 
+// SetLang set the language use with the current template system. The method must be call before Parse.
+// After call this method you can use these command in your .tmpl files:
+// 	{{lang "filename.lang" "key"}}
+// 	{{langset "set" "filename.lang" "key"}}
 func (v *View) SetLang(l *locale.Lang) {
 	v.funcsMap["lang"] = func(file, key string) string {
 		return l.Load(file, key)
