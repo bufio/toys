bcebceac601b48a1882f5c51142e799a15f1215e
nvcnvn <nguyen@open-vn.org>
2013-05-13 23:38:52 +0700

    little work with util/forms

--- util/forms/forms.go
+++ util/forms/forms.go
@@ -4,8 +4,56 @@ import (
 	"reflect"
 )
 
+var cacheStruct = map[string]*StructData{}
+
 type StructData struct {
-	Feilds    map[string]StructData
-	converter func(string) reflect.Value
-	validator func(string) error
+	Feilds map[string]*StructData
+	// converter func(string) interface{}
+	// validator func(string) error
+	IsBasic bool
+}
+
+func Prepare(i interface{}) (*StructData, error) {
+	t := reflect.TypeOf(i)
+	if t.Kind() == reflect.Struct {
+		parse(t)
+	}
+	return nil, nil
+}
+
+func parse(t reflect.Type) *StructData {
+	if v, ok := cacheStruct[t.PkgPath()+"."+t.Name()]; ok {
+		return v
+	}
+
+	sdata := &StructData{}
+
+	if (1 <= t.Kind() && t.Kind() <= 16) || t.Kind() == 24 {
+		sdata.IsBasic = true
+	} else {
+		sdata.IsBasic = false
+		numField := 0
+		if t.Kind() == reflect.Struct {
+			numField = t.NumField()
+		} else if t.Kind() == reflect.Ptr && t.Elem().Kind() == reflect.Struct {
+			t = t.Elem()
+			numField = t.NumField()
+			//TODO check the cache one more time
+		}
+		if numField > 0 {
+			sdata.Feilds = make(map[string]*StructData)
+			cacheStruct[t.PkgPath()+"."+t.Name()] = sdata
+			for i := 0; i < numField; i++ {
+				f := t.Field(i)
+				if len(f.PkgPath) == 0 { // exported field
+					sdata.Feilds[f.Name] = parse(f.Type)
+				}
+			}
+		}
+	}
+	return sdata
+}
+
+func Cache() map[string]*StructData {
+	return cacheStruct
 }
